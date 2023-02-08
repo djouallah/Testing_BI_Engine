02aae75210f9299c58dcb41eaf8e0211321d243b
Mimoune <djouallah@users.noreply.github.com>
2023-02-09 07:46:44 +1000

    Update Snowflake+SingleStore+Databricks+alloyDB.sql

--- Database/Snowflake+SingleStore+Databricks+alloyDB.sql
+++ Database/Snowflake+SingleStore+Databricks+alloyDB.sql
@@ -338,7 +338,7 @@ GROUP BY
 HAVING
     SUM(ps_supplycost * ps_availqty) > (
         SELECT
-            SUM(ps_supplycost * ps_availqty) * 0.0001
+            SUM(ps_supplycost * ps_availqty) * (0.0001/10)
             -- SUM(ps_supplycost * ps_availqty) * 1
         FROM
             partsupp,
